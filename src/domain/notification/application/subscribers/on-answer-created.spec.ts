9c8969b0bf0ce91024984a0a6bc5efe760e447bb
Maxmiller Nunes <maxmillernunes11@gmail.com>
2025-09-17 23:39:51 -0300

    feat: :sparkles: create the function that send notification after create answer event occured

--- src/domain/notification/application/subscribers/on-answer-created.spec.ts
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -1,25 +1,77 @@
+import type { MockInstance } from 'vitest'
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { OnAnswerCreated } from './on-answer-created'
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
 import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import {
+  SendNotificationUseCase,
+  type SendNotificationUseCaseRequest,
+  type SendNotificationUseCaseResponse,
+} from '../use-case/send-notification'
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { waitFor } from 'test/utils/wait-for'
 
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 
+let sendNotificationUseCase: SendNotificationUseCase
+
+let sendNotificationExecuteSpy: MockInstance<
+  (
+    data: SendNotificationUseCaseRequest
+  ) => Promise<SendNotificationUseCaseResponse>
+>
+
 describe('On Answer Created', () => {
   beforeEach(() => {
+    // Questions Repository
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository()
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
+      inMemoryQuestionAttachmentsRepository
+    )
+
     inMemoryAnswerAttachmentsRepository =
       new InMemoryAnswerAttachmentsRepository()
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
       inMemoryAnswerAttachmentsRepository
     )
+
+    // Notification repository and Send Notification UseCase
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+    sendNotificationUseCase = new SendNotificationUseCase(
+      inMemoryNotificationsRepository
+    )
+
+    // Spy when the method execute is called
+    sendNotificationExecuteSpy = vi.spyOn(sendNotificationUseCase, 'execute')
+
+    // My Subscriber need initialized
+    new OnAnswerCreated(inMemoryQuestionsRepository, sendNotificationUseCase)
   })
 
-  it('should send a notification when an answer is created', () => {
-    const onAnswerCreated = new OnAnswerCreated()
+  it('should send a notification when an answer is created', async () => {
+    // Creating the question, it necessary, because does not exists answer without question
+    const question = makeQuestion()
 
-    const answer = makeAnswer()
+    //Create answer for answer
+    const answer = makeAnswer({ questionId: question.id })
 
+    inMemoryQuestionsRepository.create(question)
+
+    // When the method create is executed, the event will be dispatch
     inMemoryAnswersRepository.create(answer)
+
+    // When the event is dispatched the method sendNewAnswerNotification of class OnAnswerCreated is executed
+    // And the method sendNotification of class SendNotificationUseCase is spied to see if he was executed
+    await waitFor(() => {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    })
   })
 })
--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import type { EventHandler } from '@/core/events/event-handler'
+import type { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import type { SendNotificationUseCase } from '../use-case/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private sendNotificationUseCase: SendNotificationUseCase
+  ) {
     this.setupSubscriptions()
   }
 
@@ -14,7 +19,21 @@ export class OnAnswerCreated implements EventHandler {
     )
   }
 
-  async sendNewAnswerNotification(event: AnswerCreatedEvent) {
-    console.log(event.answer)
+  /**
+   * Function that receive a event when a answer is created and send a new notification
+   * @param answer is the class AnswerCreatedEvent
+   */
+  async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString()
+    )
+
+    if (question) {
+      await this.sendNotificationUseCase.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em "${question.title.substring(0, 40).concat('...')}"`,
+        content: answer.excerpt,
+      })
+    }
   }
 }
